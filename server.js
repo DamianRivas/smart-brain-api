f131d690e09457abab1cd396f55c0697cb83501a
DamianRivas <drivas1993@gmail.com>
2018-05-08 00:40:52 -0700

    Return user data on signin

--- server.js
+++ server.js
@@ -42,11 +42,10 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json('success');
+    res.json(database.users[0]);
   } else {
     res.status(400).json('Error logging in');
   }
-  res.json({ name: "Damian" });
 })
 
 app.post('/register', (req, res) => {
